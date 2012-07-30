8d9dc5bff7d590f8e55e3be08f317924dfd00c20
xuefy <xuefy.com@gmail.com>
2012-07-30 23:05:49 +0800

    tweaks file loc

--- build_kernel.sh
+++ build_kernel.sh
@@ -22,9 +22,11 @@ make O=./obj/KERNEL_OBJ/ ef65l_defconfig
 make -j4 O=./obj/KERNEL_OBJ/
 
 ##############################################################################
-# Copy Kernel Image
+# Copy Kernel Image and Modules
 ##############################################################################
 cp -f ./obj/KERNEL_OBJ/arch/arm/boot/zImage ./obj/
-cp -f ./obj/KERNEL_OBJ/drivers/net/wireless/bcm4330_b2/wlan.ko ./obj/
-cp -f ./obj/KERNEL_OBJ/drivers/scsi/scsi_wait_scan.ko ./obj/
+#cp -f ./obj/KERNEL_OBJ/drivers/net/wireless/bcm4330_b2/wlan.ko ./obj/
+#cp -f ./obj/KERNEL_OBJ/drivers/scsi/scsi_wait_scan.ko ./obj/
+mkdir -p ./obj/modules/
+cp -r `find ./obj/KERNEL_OBJ/ -iname '*.ko'` ./obj/modules/
 
